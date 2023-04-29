2a4c7dbede3795f1759beb435672829c0fbf04f8
Elena Berezina <farfalina1908@gmail.com>
2023-04-30 00:47:59 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import users from './data.json';
+import Text from './Text';
 // import './index.css';
 
 // const div1 = document.createElement('div')
@@ -33,10 +34,12 @@ import users from './data.json';
 
 const Card = () => 
 users.map((el)=>{
-    return (<div className="card" style={{width: '18rem'}}>
+  return (<div key={ el.id} className="card" style={{width: '18rem'}}>
     <img src="https://media.istockphoto.com/id/1403454705/photo/a-smiling-little-puppy-of-a-light-brown-poodle-in-a-beautiful-green-meadow-is-happily-running.jpg?b=1&s=170667a&w=0&k=20&c=YZpQ16RgZj8wfGAHwAXCTtssN0WxIktXjf_2DDDGpFQ=" className="card-img-top" alt="..."/>
     <div className="card-body">
-      <h5 className="card-title">{el.name}</h5>
+        {/* <h5 className="card-title">{el.name}</h5> */}
+        {/* {Text(el.name)} */}
+        <Text name={el.name}/>
       <p className="card-text">{el.email}</p>
       <a href="{el.website}" className="btn btn-primary">{el.website}</a>
     </div>
