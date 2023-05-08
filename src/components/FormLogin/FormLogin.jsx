a6723663668122f4bb5961636553c35e1115ae20
Elena Berezina <farfalina1908@gmail.com>
2023-05-09 01:06:33 +0300

    Update FormLogin.jsx

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -5,39 +5,45 @@ class FormLogin extends Component {
       email: "",
       password: "",
       isChecked: false,
-   }
+      gender: "",
+   };
 
    handleChange = ({ target: { value, name } }) => {
       this.setState({
          [name]: value,
-      })
-   }
+      });
+   };
 
    validator = ({ target: { value, name } }) => {
       if (name === "password") {
-         !value.includes("!") && alert('Password must include "!"')
+         !value.includes("!") && alert('Password must include "!"');
       }
-   }
+   };
 
    handleSubmit = (e) => {
-      e.preventDefault()
+      e.preventDefault();
       this.props.createUser({
          email: this.state.email,
          userpassword: this.state.password,
-      })
+      });
       // this.props.close()
       this.setState({
          email: "",
          password: "",
          isChecked: false,
-      })
-   }
+      });
+   };
 
    handleCheck = ({ target: { checked } }) => {
       this.setState({
          isChecked: checked,
-      })
-   }
+      });
+   };
+
+   handleGender = ({ target: { name} }) => {
+      this.setState({ gender:name}) 
+}
+    
 
    render() {
       return (
@@ -82,8 +88,47 @@ class FormLogin extends Component {
                   checked={this.state.isChecked}
                   onChange={this.handleCheck}
                />
-               <label className="form-check-label" for="exampleCheck1">
+               <label className="form-check-label" htmlFor="exampleCheck1">
                   I agree
+               </label>
+            </div>
+            <div className="mb-3 form-check">
+               <input
+                  className="form-check-input"
+                  type="radio"
+                  name="mail"
+                  id="flexRadioDefault1"
+                  checked={this.state.gender === "mail"}
+                  onChange={this.handleGender}
+               />
+               <label className="form-check-label" htmlFor="flexRadioDefault1">
+                  Mail
+               </label>
+            </div>
+            <div className="mb-3 form-check">
+               <input
+                  className="form-check-input"
+                  type="radio"
+                  name="femail"
+                  id="flexRadioDefault2"
+                  checked={this.state.gender === "femail"}
+                  onChange={this.handleGender}
+               />
+               <label className="form-check-label" htmlFor="flexRadioDefault2">
+                  Femail
+               </label>
+            </div>
+            <div className="mb-3 form-check">
+               <input
+                  className="form-check-input"
+                  type="radio"
+                  name="other"
+                  id="flexRadioDefault3"
+                  checked={this.state.gender === "other"}
+                  onChange={this.handleGender}
+               />
+               <label className="form-check-label" htmlFor="flexRadioDefault3">
+                  Other
                </label>
             </div>
             <button
@@ -98,7 +143,7 @@ class FormLogin extends Component {
                Submit
             </button>
          </form>
-      )
+      );
    }
 }
 
