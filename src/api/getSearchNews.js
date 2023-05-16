ab304eabdacad1231a801e33a4cd14fde48f771a
Elena Berezina <farfalina1908@gmail.com>
2023-05-16 23:03:23 +0300

    update

--- src/api/getSearchNews.js
+++ src/api/getSearchNews.js
@@ -1,18 +1,5 @@
-// const BASE_URL = "https://newsapi.org/v2/everything"
-// const API_KEY = "0cfb3eb6e57c4a1d8ae951f26da2e138"
-
-// export const getSearchNews = (text) => {
-//    return fetch(`${BASE_URL}q=${text}`, {
-//       headers: {
-//          "X-Api-Key": API_KEY,
-//       },
-//    })
-//       .then((res) => res.json())
-//       .then(console.log)
-// }
-
 const BASE_URL = "https://newsapi.org/v2/everything"
-const API_KEY = "feef20bb6ec8430ab253f1d0367f9ccf"
+const API_KEY = "0cfb3eb6e57c4a1d8ae951f26da2e138"
 
 export const getSearchNews = (text) => {
    return fetch(`${BASE_URL}?q=${text}`, {
@@ -21,5 +8,7 @@ export const getSearchNews = (text) => {
       },
    })
       .then((res) => res.json())
-      .then(console.log)
+      // .then(console.log)
 }
+
+
--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -2,18 +2,29 @@ import { Component } from "react"
 import { getSearchNews } from "../../api/getSearchNews"
 
 class ContentInfo extends Component {
-   state = {}
+   state = {articles: []}
 
    componentDidUpdate(prevProps, prevState) {
       const text = this.props.searchText.trim()
       if (prevProps.searchText !== text && text) {
          //  console.log(this.props.searchText)
 
-         getSearchNews()
+         getSearchNews(text).then(({ articles }) => { this.setState ({ articles}) })
       }
    }
    render() {
-      return <></>
+      const{ articles} = this.state
+      return (
+       articles.length > 0 && (<ul className="list-group mt-3">
+               {articles.map((el) => (
+                  <li key={el.title} className="list-group-item">
+                     {el.title}
+                  </li>
+               ))}
+            </ul>)
+            
+        
+      );
    }
 }
 
